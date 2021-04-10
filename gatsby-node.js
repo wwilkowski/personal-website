b9d57fcf833a97e7ce48f06e80617bcde5d685d2
wwilkowski <wwilkowski97@gmail.com>
2021-04-10 12:33:35 +0200

    Remove unnecessary gatsby node code

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,23 +12,6 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
-
-const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
-
-exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
-  if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` })
-    createNodeField({
-      node,
-      name: `slug`,
-      value: slug,
-    })
-  }
-}
-
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
 
