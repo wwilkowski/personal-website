f93c47707384265b7b28518d808ac56550c0d12d
wwilkowski <wwilkowski97@gmail.com>
2021-04-10 12:55:20 +0200

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-This website is built on top of [Gatsby Starter Julia](https://www.gatsbyjs.com/starters/niklasmtj/gatsby-starter-julia). 
--- gatsby-browser.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-import { ThemeProvider } from "./src/components/ThemeContext"
-export const wrapRootElement = ({ element }) => (
-  <ThemeProvider>{element}</ThemeProvider>
-)
